3a6f74b022f89217e0222a406275b59ccac0f67a
Amantur Ergeshov <amantur.e@4taleproduction.com>
2024-04-25 08:20:18 +0600

    Add KorgoolsCountUI

---
+++ DiplomProject/Assets/Scripts/ToguzKorgoolScripts/S_UI_KorgoolsCount.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class S_UI_KorgoolsCount : MonoBehaviour
+{
+    public S_Lunka Lunka;
+    public Text korgoolsCountText; // ссылка на компонент текста
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Проверяем, что ссылка на компонент текста присутствует
+        if (korgoolsCountText == null)
+        {
+            Debug.LogError("Text component reference is missing!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Проверяем, что ссылка на объект Lunka присутствует
+        if (Lunka != null && korgoolsCountText != null)
+        {
+            // Обновляем текстовое значение компонента текста с помощью свойства text
+            korgoolsCountText.text = Lunka.KorgoolsCount.ToString();
+        }
+        else
+        {
+            Debug.LogWarning("Lunka reference or Text component reference is missing!");
+        }
+    }
+}
