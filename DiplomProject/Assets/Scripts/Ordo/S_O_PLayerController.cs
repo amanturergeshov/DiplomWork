52ac0546b31253d87763ad75ee4143cd915111af
amanturergeshov <amantur.ergeshov001@gmail.com>
2024-05-29 20:47:11 +0600

    littleUpdate

--- DiplomProject/Assets/Scripts/Ordo/S_O_PLayerController.cs
+++ DiplomProject/Assets/Scripts/Ordo/S_O_PLayerController.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.Tracing;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
